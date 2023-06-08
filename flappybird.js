93a9b55eb09eab13b6ec7ab7f41c983002560719
ahmedraza90 <34ahmed.raza@gmail.com>
2023-06-08 21:18:00 +0500

    wallet authentication

--- flappybird.js
+++ flappybird.js
@@ -60,6 +60,7 @@ window.onload = function () {
         requestAnimationFrame(update);
         setInterval(placePipes, 1500); //every 1.5 seconds
         document.addEventListener("keydown", moveBird);
+        document.addEventListener("touchstart", moveBird);
     }
     function connectWallet() {
         // Request account access
